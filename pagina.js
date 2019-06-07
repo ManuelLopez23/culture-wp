44811b609cc215db88a02f725a100b8db94bf263
ManuelLopez23 <51493684+ManuelLopez23@users.noreply.github.com>
2019-06-07 13:41:57 -0600

    Added Files

---
+++ pagina.js
@@ -0,0 +1,111 @@
+function videoA(){
+    document.getElementById("video").src="https://www.youtube.com/embed/93M1QtYDtpU?autoplay=1";
+}
+function videoJM(){
+    document.getElementById("video").src="https://www.youtube.com/embed/mQ055hHdxbE?autoplay=1";
+}
+function videoJC(){
+    document.getElementById("video").src="https://www.youtube.com/embed/ZiADuDjueJc?autoplay=1";
+}
+function videoFO(){
+    document.getElementById("video").src="https://www.youtube.com/embed/r4l9bFqgMaQ?autoplay=1";
+}
+function videoTS(){
+    document.getElementById("video").src="https://www.youtube.com/embed/tBfWb5IrbOI?autoplay=1";
+}
+function audio1(){
+    document.getElementById("audio").src="https://open.spotify.com/embed/track/62vpWI1CHwFy7tMIcSStl8";
+}
+function audio2(){
+    document.getElementById("audio").src="https://open.spotify.com/embed/track/00GOPLxW4PGQuUYdPJh8K1";  
+}
+function audio3(){
+    document.getElementById("audio").src="https://open.spotify.com/embed/track/4uQ7wYsuL0DryknoDc11Hk";
+}
+function audio4(){
+    document.getElementById("audio").src="https://open.spotify.com/embed/track/51EC3I1nQXpec4gDk0mQyP";
+}
+function audio5(){
+    document.getElementById("audio").src="https://open.spotify.com/embed/track/6NyhBA9M6KK0fuVsojxL00";
+}
+function audio6(){
+    document.getElementById("audio").src="https://open.spotify.com/embed/track/2tPcTFiQF9MbVUyjZ3zDhA";
+}
+function audio7(){
+    document.getElementById("audio").src="https://open.spotify.com/embed/track/6vnfObZ4Ys70SBAtti1xZ9";
+}
+function audio8(){
+    document.getElementById("audio").src="https://open.spotify.com/embed/track/58TaFP5j5i9swOf584yon2";
+}
+function audio9(){
+    document.getElementById("audio").src="https://open.spotify.com/embed/track/6GnhWMhgJb7uyiiPEiEkDA";
+}
+function audio10(){
+    document.getElementById("audio").src="https://open.spotify.com/embed/track/7eqoqGkKwgOaWNNHx90uEZ";
+}
+function audio11(){
+    document.getElementById("audio").src="https://open.spotify.com/embed/track/2z3joGXrdGL2iEjIKQTiN1";
+}
+function info1(){
+    document.getElementById("infoh").innerHTML="J. Cole";
+    document.getElementById("infop").innerHTML="Jermaine Lamar Cole, known professionally as J. Cole, is an American rapper, singer, songwriter and record producer. Born on a military base in Germany but raised in Fayetteville, North Carolina, Cole initially gained recognition as a rapper following the release of his debut mixtape, The Come Up, in early 2007";
+    document.getElementById("info").style.backgroundColor= "rgb(31, 59, 95)";
+}
+function info2(){
+    document.getElementById("infoh").innerHTML="Childish Gambino";
+    document.getElementById("infop").innerHTML="Donald McKinley Glover Jr. is an American actor, comedian, writer, producer, director, musician, and DJ. He performs music under the stage name Childish Gambino and as a DJ under the name mcDJ";
+    document.getElementById("info").style.backgroundColor= "rgb(172, 79, 3)";
+}
+function info3(){
+    document.getElementById("infoh").innerHTML="Vince Staples";
+    document.getElementById("infop").innerHTML="Vincent Jamal Staples is an American rapper, singer, songwriter and actor. He is a member of the hip hop trio Cutthroat Boyz alongside fellow Californian rappers Aston Matthews and Joey Fatts. Staples was once a close associate of Odd Future, in particular Mike G and Earl Sweatshirt.";
+    document.getElementById("info").style.backgroundColor= "rgb(46, 46, 46)";
+}
+function info4(){
+    document.getElementById("infoh").innerHTML="Travis Scott";
+    document.getElementById("infop").innerHTML="Jacques Berman Webster II, known professionally as Travis Scott, is an American rapper, singer, songwriter and record producer. In 2012, Scott signed his first major-label deal with Epic Records";
+    document.getElementById("info").style.backgroundColor= "rgb(192, 150, 59)";
+}
+function info5(){
+    document.getElementById("infoh").innerHTML="SOB x RBE";
+    document.getElementById("infop").innerHTML="The group currently consists of 4 members: Slimmy B (Jabbar Brown), Yhung T.O. (Juwon Lee), DaBoii (Wayman Barrow) and Lul G (George Harris). The group has released four complete projects: a self-titled mixtape in 2017, a debut album Gangin in 2018, an EP with Big Money and a second album Gangin II in late 2018. The group found mainstream attention after being featured on the track Paramedic! for the 2018 superhero film Black Panther. The group has gone on multiple tours around the United States and have been interviewed multiple times by The Fader and Billboard.";
+    document.getElementById("info").style.backgroundColor= "rgb(46, 46, 46)";
+    
+}
+function info6(){
+    document.getElementById("infoh").innerHTML="Mac Miller";
+    document.getElementById("infop").innerHTML="Malcolm James McCormick, known professionally as Mac Miller, was an American rapper, songwriter, and record producer. Born in Pittsburgh, Pennsylvania, Miller began his career in the city's hip hop scene in 2007, at the age of fifteen.";
+    document.getElementById("info").style.backgroundColor= "rgb(96, 130, 194)";
+}
+function info7(){
+    document.getElementById("infoh").innerHTML="Frank Ocean";
+    document.getElementById("infop").innerHTML="Frank Ocean is an American singer, songwriter, rapper, record producer and photographer. Ocean began his musical career as a ghostwriter, prior to joining hip hop collective Odd Future in 2010, and the following year, released Nostalgia, Ultra, his debut mixtape.";
+    document.getElementById("info").style.backgroundColor= "rgb(128, 173, 210)";
+}
+function album1(){
+    document.getElementById("album").src="https://open.spotify.com/embed/album/7viNUmZZ8ztn2UB4XB3jIL";
+}
+function album2(){
+    document.getElementById("album").src="https://open.spotify.com/embed/album/4GNIhgEGXzWGAefgN5qjdU";
+}
+function album3(){
+    document.getElementById("album").src="https://open.spotify.com/embed/album/4Csoz10NhNJOrCTUoPBdUD";
+}
+function album4(){
+    document.getElementById("album").src="https://open.spotify.com/embed/album/4PWBTB6NYSKQwfo79I3prg";
+}
+function album5(){
+    document.getElementById("album").src="https://open.spotify.com/embed/album/3pLdWdkj83EYfDN6H2N8MR";
+}
+function album6(){
+    document.getElementById("album").src="https://open.spotify.com/embed/album/41GuZcammIkupMPKH2OJ6I";
+}
+function album7(){
+    document.getElementById("album").src="https://open.spotify.com/embed/album/2Tyx5dLhHYkx6zeAdVaTzN";
+}
+function album8(){
+    document.getElementById("album").src="https://open.spotify.com/embed/album/3mH6qwIy9crq0I9YQbOuDf";
+}
+function album9(){
+    document.getElementById("album").src="https://open.spotify.com/embed/album/3CCnGldVQ90c26aFATC1PW";
+}
